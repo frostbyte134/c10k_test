fd183d82e067d26549db721195a49c328f813808
Nailbrainz-ubuntu <nailbrainz@gmail.com>
2020-08-03 19:55:22 +0900

    cleaning server codes for select()

--- client.c
+++ client.c
@@ -5,55 +5,54 @@
 #include "netinet/in.h"
  
 #define BUF_LEN 128
- 
+char buf[BUF_LEN+1];
+
 void main(int argc, char *argv[])
 {
-        int s, n;
-        char *haddr;
-        struct sockaddr_in server_addr;
-        //struct sockaddr_in server_addr : 서버의 소켓주소 구조체
-        char buf[BUF_LEN+1];
+	int s, n;
+	char *haddr;
+	struct sockaddr_in server_addr;
+	
+	if(argc != 2)
+	{
+		printf("usage : %s ip_Address\n", argv[0]);
+		exit(0);
+	}
+	haddr = argv[1];
 
-        if(argc != 2)
-        {
-                printf("usage : %s ip_Address\n", argv[0]);
-                exit(0);
-        }
-        haddr = argv[1];
- 
-        if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-        {//소켓 생성과 동시에 소켓 생성 유효검사
-                printf("can't create socket\n");
-                exit(0);
-        }
- 
-        bzero((char *)&server_addr, sizeof(server_addr));
-        //서버의 소켓주소 구조체 server_addr을 NULL로 초기화
- 
-        server_addr.sin_family = AF_INET;
-        //주소 체계를 AF_INET 로 선택
-        server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-        //32비트의 IP주소로 변환
-        server_addr.sin_port = htons(10102);
-        
-        //daytime 서비스 포트 번호
-        printf("[C10K][DWK] trying connect!\n");
-        if(connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
-        {//서버로 연결요청
-                printf("can't connect.\n");
-                exit(0);
-        }
-        printf("[C10K][DWK] trying connect!\n");
-        const char* buffer = "abcdefghijklmnopqrstuv";
-        unsigned int cnt = 0;
-        while(1){
-                if(cnt % 1000000 == 0)
-                        printf("sending %s\n", buffer);
-                cnt++;
-                send(s, buffer, strlen(buffer), 0);
-        }
-        send(s, buffer, 0, 0);
-        close(s);
-        printf("[C10K][SERV] closing client\n");
-        //사용이 완료된 소켓을 닫기
+	if((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+	{//소켓 생성과 동시에 소켓 생성 유효검사
+		printf("can't create socket\n");
+		exit(0);
+	}
+
+	bzero((char *)&server_addr, sizeof(server_addr));
+	//서버의 소켓주소 구조체 server_addr을 NULL로 초기화
+
+	server_addr.sin_family = AF_INET;
+	//주소 체계를 AF_INET 로 선택
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	//32비트의 IP주소로 변환
+	server_addr.sin_port = htons(10102);
+	
+	//daytime 서비스 포트 번호
+	printf("[C10K][DWK] trying connect!\n");
+	if(connect(s, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+	{//서버로 연결요청
+		printf("can't connect.\n");
+		exit(0);
+	}
+	printf("[C10K][DWK] trying connect!\n");
+	const char* buffer = "abcdefghijklmnopqrstuv";
+	unsigned int cnt = 0;
+	while(1){
+		if(cnt % 1000000 == 0)
+				printf("sending %s\n", buffer);
+		cnt++;
+		send(s, buffer, strlen(buffer), 0);
+	}
+	send(s, buffer, 0, 0);
+	close(s);
+	printf("[C10K][SERV] closing client\n");
+	//사용이 완료된 소켓을 닫기
 }
--- server.c
+++ server.c
@@ -6,62 +6,48 @@
 #include "sys/socket.h"
 #include "netinet/in.h"
 #include <sys/select.h>
-//소켓 프로그래밍에 사용될 헤더파일 선언
- 
-#define BUF_LEN 128
-//메시지 송수신에 사용될 버퍼 크기를 선언
- 
+
+#define SERV_PORT (24424u)
+#define BUF_SIZE (1024u)
+char buffer[BUF_SIZE];
+
 int main(int argc, char *argv[])
 {
-    char buffer[BUF_LEN];
     struct sockaddr_in server_addr, client_addr;
-    char temp[20];
-    int server_fd, client_fd;
-    int len, msg_size;
- 
-    if(argc != 2)
+    int serv_fd;
+    
+    if((serv_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
     {
-        printf("usage : %s [port]\n", argv[0]);
-        exit(0);
+        fprintf(stderr, "Server : Can't open stream socket\n");
+        exit(-1);
     }
- 
-    if((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1)
-    {// 소켓 생성
-        printf("Server : Can't open stream socket\n");
-        exit(0);
-    }
-    memset(&server_addr, 0x00, sizeof(server_addr));
-    //server_Addr 을 NULL로 초기화
- 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(atoi(argv[1]));
-    printf("[C10K][SERV] port =%s\n", argv[1]);
-    //server_addr 셋팅
- 
+    server_addr.sin_port = htons(SERV_PORT);
+    printf("[C10K][SERV] port = %u\n", SERV_PORT);
+    
     printf("[C10K][SERV] trying bind!\n");
-    if(bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <0)
-    {//bind() 호출
-        printf("Server : Can't bind local address.\n");
-        exit(0);
+	//binds the port and serv_fd
+    if(bind(serv_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <0)
+    {
+        fprintf(stderr, "Server : Can't bind local address.\n");
+        exit(-1);
     }
     
 
-	printf("[C10K][SERV] bind finished!\n");
-    if(listen(server_fd, 5) < 0)
-    {//소켓을 수동 대기모드로 설정
-        printf("Server : Can't listening connect.\n");
-        exit(0);
+	printf("[C10K][SERV] bind finished. Beginning to listen...\n");
+    if(listen(serv_fd, 5) < 0)
+    {
+        fprintf(stderr, "[C10K][SERV]: Can't listening connect.\n");
+        exit(-1);
     }
 	
 	fd_set reads, cpy_reads;
 	FD_ZERO(&reads);
-	FD_SET(server_fd, &reads);
-	int fd_max = server_fd; //for each connected clients, assign socket file descriptor
+	FD_SET(serv_fd, &reads);
+	int fd_max = serv_fd; //for each connected clients, assign socket file descriptor
 	int fd_num = 0;
-    memset(buffer, 0x00, sizeof(buffer));
-    printf("Server : wating connection request.\n");
-    len = sizeof(client_addr);
+    printf("[C10K][SERV]: wating connection request.\n");
 	struct timeval timeout;
 	unsigned int cnt = 0;
     while(1)
@@ -71,8 +57,7 @@ int main(int argc, char *argv[])
 		timeout.tv_sec = 5;
 		timeout.tv_usec = 50000;
 		if((fd_num = select(fd_max+1, &cpy_reads, 0, 0, &timeout)) == -1){
-			printf("fd_num : %d\n", fd_num);
-			perror("select() error");
+			fprintf(stderr, "[C10K][SERV] fd_num : %d select() error", fd_num);
 			break;
 		}
 		if(fd_num == 0)
@@ -80,32 +65,33 @@ int main(int argc, char *argv[])
 
 		for(int i = 0; i<=fd_max; i++){
 			if(FD_ISSET(i, &cpy_reads)){
-				if(i == server_fd){
-					client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
-					FD_SET(client_fd, &reads);
-					if(fd_max < client_fd){
-						fd_max = client_fd;
+				if(i == serv_fd){
+					int sock_fd = 0, add_struct_size = 0;
+					sock_fd = accept(serv_fd, (struct sockaddr *)&client_addr, &add_struct_size);
+					FD_SET(sock_fd, &reads);
+					if(fd_max < sock_fd){
+						fd_max = sock_fd;
 					}
 					printf("[C10K][SERV] accept!\n");
-					if(client_fd < 0)
+					if(sock_fd < 0)
 					{
-						perror("Server: accept failed.\n");
-						exit(0);
+						fprintf(stderr, "[C10K][SERV]: accept failed.\n");
+						exit(-1);
 					}
-					printf("connected socket %d\n", client_fd);
 				}else{
 					if(cnt % 1000 == 0)
-						printf("processed socket %d\n", i);
-					msg_size = read(i, buffer, 1024);
+						printf("[C10K][SERV] processed socket %d\n", i);
+					int msg_size = read(i, buffer, BUF_SIZE);
 					if(msg_size == 0){
 						FD_CLR(i, &reads);
 				        close(i);
-						printf("Server : %d client closed.\n", i);
+						printf("[C10K][SERV] Server : %d client closed.\n", i);
 					}
 				}
 			}
 		}
     }
-    close(server_fd);
+    close(serv_fd);
+	printf("[C10K][SERV], closed socket successfully (unreachable, perahps)\n");
     return 0;
 }
