d0959c2691d6d455374cb07a6416f8e8ada55e85
Nailbrainz-ubuntu <nailbrainz@gmail.com>
2020-08-03 22:17:03 +0900

    indent

--- src/main.c
+++ src/main.c
@@ -12,32 +12,32 @@ uint8_t buffer[BUF_SIZE];
 
 int main(int argc, char *argv[])
 {
-    struct sockaddr_in server_addr;
-    int serv_fd;
+	struct sockaddr_in server_addr;
+	int serv_fd;
 
-    if( (serv_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
-        fprintf(stderr, "[SERV] : Can't open stream socket\n");
-        exit(-1);
-    }
+	if( (serv_fd = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
+		fprintf(stderr, "[SERV] : Can't open stream socket\n");
+		exit(-1);
+	}
 	
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    server_addr.sin_port = htons(SERV_PORT);
-    printf("[SERV] port = %u, trying bind!\n", SERV_PORT);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	server_addr.sin_port = htons(SERV_PORT);
+	printf("[SERV] port = %u, trying bind!\n", SERV_PORT);
 	//binds the port and serv_fd
-    if(bind(serv_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) <0)
-    {
-        fprintf(stderr, "[SERV] : Can't bind local address.\n");
-        exit(-1);
-    }
-    
+	if(bind(serv_fd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in)) <0)
+	{
+		fprintf(stderr, "[SERV] : Can't bind local address.\n");
+		exit(-1);
+	}
+	
 
 	printf("[SERV] bind finished. Beginning to listen...\n");
-    if(listen(serv_fd, 5) < 0)
-    {
-        fprintf(stderr, "[SERV]: Can't listening connect.\n");
-        exit(-1);
-    }
+	if(listen(serv_fd, 5) < 0)
+	{
+		fprintf(stderr, "[SERV]: Can't listening connect.\n");
+		exit(-1);
+	}
 	
 
 
@@ -47,5 +47,5 @@ int main(int argc, char *argv[])
 	printf("[SERV] found server socket. Beginning to run the main c10k procedure...\n");
 	appObj.server_engine(serv_fd, buffer);
 
-    return 0;
+	return 0;
 }
